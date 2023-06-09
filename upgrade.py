0fe82633b419295e990eb31034a7a74ca8a380ca
Norah (Jo) Vii <70073104+norahvii@users.noreply.github.com>
2023-06-09 13:14:54 -0500

    Update upgrade.py

--- upgrade.py
+++ upgrade.py
@@ -6,60 +6,40 @@ import os
 upgrade_output = subprocess.check_output(['winget', 'upgrade']).decode('utf-8')
 lines = upgrade_output.split('\n')
 
-for i in lines:
-	header = lines[0]
 
-prefix_char = "I"
-prefix_count = 0
+package_ids = []
 
-suffix_char = "V"
-suffix_count = 0
+pattern = r'\b(?![0-9]+\.[0-9]+\b)\w+\.\w+\b'
+matches = re.findall(pattern, str(lines))
 
-for i in header:
-    prefix_count += 1
-    print(str(i))
-    if prefix_char in i:
-        break
+print("---------------------------------------")
 
-print(prefix_count)
+print("The following packages can be upgraded:")
 
+for match in matches:
+    print(match)
+    package_ids.append(match)
 
-for i in header:
-    suffix_count += 1
-    print(str(i))
-    if suffix_char in i:
-        break
+print("---------------------------------------")
 
-print(suffix_count)
+# Read the exclusions from the "exclusions.txt" file
+exclusions = set()
+exclusions_file = "exclusions.txt"
+if os.path.exists(exclusions_file):
+    with open(exclusions_file, 'r') as file:
+        for line in file:
+            exclusion = line.strip()
+            if exclusion:
+                exclusions.add(exclusion)
+                print(f"Added {exclusion} to list of exclusions.")
 
+# Filter out excluded package IDs
+remaining_ids = [package_id for package_id in package_ids if package_id not in exclusions]
 
-
-for line in lines[2:-1]:
-	print(line[prefix_count:])
-
-
-# for i in package_ids:
-#     print(i)
-
-#
-# # Read the exclusions from the "exclusions.txt" file
-# exclusions = set()
-# exclusions_file = "exclusions.txt"
-# if os.path.exists(exclusions_file):
-#     with open(exclusions_file, 'r') as file:
-#         for line in file:
-#             exclusion = line.strip()
-#             if exclusion:
-#                 exclusions.add(exclusion)
-#                 print(f"Added {exclusion} to list of exclusions.")
-#
-# # Filter out excluded package IDs
-# remaining_ids = [package_id for package_id in package_ids if package_id not in exclusions]
-#
-# # Perform "winget upgrade" for each remaining package ID
-# for package_id in remaining_ids:
-#     try:
-#         print(f"Attempting update on {package_id}...")
-#         subprocess.call(['winget', 'upgrade', package_id])
-#     except subprocess.CalledProcessError as e:
-#         print(f"Error occurred while upgrading package {package_id}: {str(e)}")
+# Perform "winget upgrade" for each remaining package ID
+for package_id in remaining_ids:
+    try:
+        print(f"Attempting update on {package_id}...")
+        subprocess.call(['winget', 'upgrade', package_id])
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred while upgrading package {package_id}: {str(e)}")
